f7acf616d75c292bcbb881cf4ecb96152af39741
Graham <github@grahamhoyle.co.uk>
2020-05-05 18:12:12 +0100

    Unsafe mathods marked unsafe

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ export default class NavigationBar extends Component {
     this.customizeStatusBar();
   }
 
-  componentWillReceiveProps() {
+  UNSAFE_componentWillReceiveProps() {
     this.customizeStatusBar();
   }
 
